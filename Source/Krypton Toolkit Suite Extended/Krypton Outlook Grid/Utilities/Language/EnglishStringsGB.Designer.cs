de3e4b492d8fb5432c0e565b9bb9efe3cd5cefd4
Peter Wagner <peterwagner@live.co.uk>
2018-02-02 12:15:43 +0000

    * French strings

--- Source/Krypton Toolkit Suite Extended/Krypton Outlook Grid/Utilities/Language/EnglishStringsGB.Designer.cs
+++ Source/Krypton Toolkit Suite Extended/Krypton Outlook Grid/Utilities/Language/EnglishStringsGB.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace KryptonOutlookGrid.Utilities {
+namespace KryptonOutlookGrid.Utilities.Language {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace KryptonOutlookGrid.Utilities {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("KryptonOutlookGrid.Utilities.EnglishStringsGB", typeof(EnglishStringsGB).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("KryptonOutlookGrid.Utilities.Language.EnglishStringsGB", typeof(EnglishStringsGB).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- Source/Krypton Toolkit Suite Extended/Krypton Outlook Grid/Utilities/Language/EnglishStringsUS.Designer.cs
+++ Source/Krypton Toolkit Suite Extended/Krypton Outlook Grid/Utilities/Language/EnglishStringsUS.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace KryptonOutlookGrid.Utilities {
+namespace KryptonOutlookGrid.Utilities.Language {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace KryptonOutlookGrid.Utilities {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("KryptonOutlookGrid.Utilities.EnglishStringsUS", typeof(EnglishStringsUS).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("KryptonOutlookGrid.Utilities.Language.EnglishStringsUS", typeof(EnglishStringsUS).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
