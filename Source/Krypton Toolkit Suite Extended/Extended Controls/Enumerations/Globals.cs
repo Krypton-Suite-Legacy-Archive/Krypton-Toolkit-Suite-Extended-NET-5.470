e6b351255f091bdee0e60cc95311be4185924dd7
Peter Wagner <peterwagner@live.co.uk>
2018-02-05 18:19:02 +0000

    * Global enums

---
+++ Source/Krypton Toolkit Suite Extended/Extended Controls/Enumerations/Globals.cs
@@ -0,0 +1,49 @@
+﻿namespace ExtendedControls.Enumerations
+{
+    /// <summary>
+    /// Messagebox icon
+    /// </summary>
+    public enum KryptonMessageBoxIcon
+    {
+        /// <summary>
+        /// A hand icon.
+        /// </summary>
+        HAND = 0,
+        /// <summary>
+        /// A question icon.
+        /// </summary>
+        QUESTION = 1,
+        /// <summary>
+        /// A exclamation icon.
+        /// </summary>
+        EXCLAMATION = 2,
+        /// <summary>
+        /// A asterisk icon.
+        /// </summary>
+        ASTERISK = 3,
+        /// <summary>
+        /// A stop icon.
+        /// </summary>
+        STOP = 4,
+        /// <summary>
+        /// A error icon.
+        /// </summary>
+        ERROR = 5,
+        /// <summary>
+        /// A warning icon.
+        /// </summary>
+        WARNING = 6,
+        /// <summary>
+        /// A information icon.
+        /// </summary>
+        INFORMATION = 7,
+        /// <summary>
+        /// A custom icon (must be X x X).
+        /// </summary>
+        CUSTOM = 8,
+        /// <summary>
+        /// No icon set.
+        /// </summary>
+        NONE = 9
+    }
+}
