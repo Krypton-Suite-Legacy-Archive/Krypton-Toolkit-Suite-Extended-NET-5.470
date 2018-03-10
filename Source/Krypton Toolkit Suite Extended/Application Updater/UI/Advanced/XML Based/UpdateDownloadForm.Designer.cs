f3e96999e02b8ea948d40655d4d1348d14f121c8
Wagnerp <dev.wagnerp@outlook.com>
2018-03-10 18:36:33 +0000

    * Minor edits

--- Source/Krypton Toolkit Suite Extended/Application Updater/UI/Advanced/XML Based/UpdateDownloadForm.Designer.cs
+++ Source/Krypton Toolkit Suite Extended/Application Updater/UI/Advanced/XML Based/UpdateDownloadForm.Designer.cs
@@ -28,40 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UpdateDownloadForm));
             this.kpnlBackground = new ComponentFactory.Krypton.Toolkit.KryptonPanel();
             this.kuacbtnInstallUpdate = new ExtendedControls.ExtendedToolkit.Controls.KryptonUACShieldButton();
             this.kbtnCancel = new ComponentFactory.Krypton.Toolkit.KryptonButton();
-            this.klblFilename = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
-            this.kryptonLabel1 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.klblDownloadingFrom = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.klblDownloadingTo = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
             this.klblDownloaded = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
-            this.kryptonLabel2 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
-            this.klblMD5 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.klblCurrentSpeed = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.klblDownloadPercent = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.pbDownloadIndicator = new System.Windows.Forms.ProgressBar();
+            this.kMan = new ComponentFactory.Krypton.Toolkit.KryptonManager(this.components);
+            this.ttHelp = new System.Windows.Forms.ToolTip(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.kpnlBackground)).BeginInit();
             this.kpnlBackground.SuspendLayout();
             this.SuspendLayout();
             // 
             // kpnlBackground
             // 
-            this.kpnlBackground.Controls.Add(this.progressBar1);
-            this.kpnlBackground.Controls.Add(this.klblMD5);
-            this.kpnlBackground.Controls.Add(this.kryptonLabel2);
+            this.kpnlBackground.Controls.Add(this.pbDownloadIndicator);
+            this.kpnlBackground.Controls.Add(this.klblDownloadPercent);
+            this.kpnlBackground.Controls.Add(this.klblCurrentSpeed);
             this.kpnlBackground.Controls.Add(this.klblDownloaded);
-            this.kpnlBackground.Controls.Add(this.kryptonLabel1);
-            this.kpnlBackground.Controls.Add(this.klblFilename);
+            this.kpnlBackground.Controls.Add(this.klblDownloadingTo);
+            this.kpnlBackground.Controls.Add(this.klblDownloadingFrom);
             this.kpnlBackground.Controls.Add(this.kuacbtnInstallUpdate);
             this.kpnlBackground.Controls.Add(this.kbtnCancel);
             this.kpnlBackground.Dock = System.Windows.Forms.DockStyle.Fill;
             this.kpnlBackground.Location = new System.Drawing.Point(0, 0);
             this.kpnlBackground.Name = "kpnlBackground";
-            this.kpnlBackground.Size = new System.Drawing.Size(800, 450);
+            this.kpnlBackground.Size = new System.Drawing.Size(800, 298);
             this.kpnlBackground.TabIndex = 0;
             // 
             // kuacbtnInstallUpdate
             // 
             this.kuacbtnInstallUpdate.Enabled = false;
-            this.kuacbtnInstallUpdate.Location = new System.Drawing.Point(472, 400);
+            this.kuacbtnInstallUpdate.Location = new System.Drawing.Point(472, 247);
             this.kuacbtnInstallUpdate.Name = "kuacbtnInstallUpdate";
             this.kuacbtnInstallUpdate.Size = new System.Drawing.Size(199, 38);
             this.kuacbtnInstallUpdate.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -71,32 +74,32 @@
             // 
             // kbtnCancel
             // 
-            this.kbtnCancel.Location = new System.Drawing.Point(677, 400);
+            this.kbtnCancel.Location = new System.Drawing.Point(677, 247);
             this.kbtnCancel.Name = "kbtnCancel";
             this.kbtnCancel.Size = new System.Drawing.Size(111, 38);
             this.kbtnCancel.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.kbtnCancel.TabIndex = 1;
             this.kbtnCancel.Values.Text = "&Cancel";
             // 
-            // klblFilename
+            // klblDownloadingFrom
             // 
-            this.klblFilename.AutoSize = false;
-            this.klblFilename.Location = new System.Drawing.Point(12, 12);
-            this.klblFilename.Name = "klblFilename";
-            this.klblFilename.Size = new System.Drawing.Size(776, 26);
-            this.klblFilename.StateCommon.ShortText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.klblFilename.TabIndex = 3;
-            this.klblFilename.Values.Text = "These are the checksum values for the file: {0}";
+            this.klblDownloadingFrom.AutoSize = false;
+            this.klblDownloadingFrom.Location = new System.Drawing.Point(12, 12);
+            this.klblDownloadingFrom.Name = "klblDownloadingFrom";
+            this.klblDownloadingFrom.Size = new System.Drawing.Size(776, 26);
+            this.klblDownloadingFrom.StateCommon.ShortText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.klblDownloadingFrom.TabIndex = 3;
+            this.klblDownloadingFrom.Values.Text = "Downloading from: {0}";
             // 
-            // kryptonLabel1
+            // klblDownloadingTo
             // 
-            this.kryptonLabel1.AutoSize = false;
-            this.kryptonLabel1.Location = new System.Drawing.Point(12, 73);
-            this.kryptonLabel1.Name = "kryptonLabel1";
-            this.kryptonLabel1.Size = new System.Drawing.Size(776, 26);
-            this.kryptonLabel1.StateCommon.ShortText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.kryptonLabel1.TabIndex = 4;
-            this.kryptonLabel1.Values.Text = "These are the checksum values for the file: {0}";
+            this.klblDownloadingTo.AutoSize = false;
+            this.klblDownloadingTo.Location = new System.Drawing.Point(12, 73);
+            this.klblDownloadingTo.Name = "klblDownloadingTo";
+            this.klblDownloadingTo.Size = new System.Drawing.Size(776, 26);
+            this.klblDownloadingTo.StateCommon.ShortText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.klblDownloadingTo.TabIndex = 4;
+            this.klblDownloadingTo.Values.Text = "Downloading to: {0}";
             // 
             // klblDownloaded
             // 
@@ -107,36 +110,36 @@
             this.klblDownloaded.TabIndex = 5;
             this.klblDownloaded.Values.Text = "Downloaded: {0} MB of {1} MB";
             // 
-            // kryptonLabel2
+            // klblCurrentSpeed
             // 
-            this.kryptonLabel2.Location = new System.Drawing.Point(538, 127);
-            this.kryptonLabel2.Name = "kryptonLabel2";
-            this.kryptonLabel2.Size = new System.Drawing.Size(250, 26);
-            this.kryptonLabel2.StateCommon.ShortText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.kryptonLabel2.TabIndex = 6;
-            this.kryptonLabel2.Values.Text = "Downloaded: {0} MB of {1} MB";
+            this.klblCurrentSpeed.Location = new System.Drawing.Point(538, 127);
+            this.klblCurrentSpeed.Name = "klblCurrentSpeed";
+            this.klblCurrentSpeed.Size = new System.Drawing.Size(189, 26);
+            this.klblCurrentSpeed.StateCommon.ShortText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.klblCurrentSpeed.TabIndex = 6;
+            this.klblCurrentSpeed.Values.Text = "Current Speed: {0}KB/s";
             // 
-            // klblMD5
+            // klblDownloadPercent
             // 
-            this.klblMD5.Location = new System.Drawing.Point(736, 204);
-            this.klblMD5.Name = "klblMD5";
-            this.klblMD5.Size = new System.Drawing.Size(52, 26);
-            this.klblMD5.StateCommon.ShortText.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.klblMD5.TabIndex = 7;
-            this.klblMD5.Values.Text = "100%";
+            this.klblDownloadPercent.Location = new System.Drawing.Point(736, 204);
+            this.klblDownloadPercent.Name = "klblDownloadPercent";
+            this.klblDownloadPercent.Size = new System.Drawing.Size(44, 26);
+            this.klblDownloadPercent.StateCommon.ShortText.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.klblDownloadPercent.TabIndex = 7;
+            this.klblDownloadPercent.Values.Text = "{0}%";
             // 
-            // progressBar1
+            // pbDownloadIndicator
             // 
-            this.progressBar1.Location = new System.Drawing.Point(12, 204);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(718, 26);
-            this.progressBar1.TabIndex = 8;
+            this.pbDownloadIndicator.Location = new System.Drawing.Point(12, 204);
+            this.pbDownloadIndicator.Name = "pbDownloadIndicator";
+            this.pbDownloadIndicator.Size = new System.Drawing.Size(718, 26);
+            this.pbDownloadIndicator.TabIndex = 8;
             // 
             // UpdateDownloadForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 298);
             this.Controls.Add(this.kpnlBackground);
             this.Name = "UpdateDownloadForm";
             this.Text = "Downloading Update";
@@ -153,11 +156,13 @@
         private ComponentFactory.Krypton.Toolkit.KryptonPanel kpnlBackground;
         private ComponentFactory.Krypton.Toolkit.KryptonButton kbtnCancel;
         private ExtendedControls.ExtendedToolkit.Controls.KryptonUACShieldButton kuacbtnInstallUpdate;
-        private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel2;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel klblCurrentSpeed;
         private ComponentFactory.Krypton.Toolkit.KryptonLabel klblDownloaded;
-        private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel1;
-        private ComponentFactory.Krypton.Toolkit.KryptonLabel klblFilename;
-        private System.Windows.Forms.ProgressBar progressBar1;
-        private ComponentFactory.Krypton.Toolkit.KryptonLabel klblMD5;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel klblDownloadingTo;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel klblDownloadingFrom;
+        private System.Windows.Forms.ProgressBar pbDownloadIndicator;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel klblDownloadPercent;
+        private ComponentFactory.Krypton.Toolkit.KryptonManager kMan;
+        private System.Windows.Forms.ToolTip ttHelp;
     }
 }
