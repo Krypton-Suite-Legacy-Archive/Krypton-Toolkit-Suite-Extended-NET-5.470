d1205e14905bb3f4875a7adf9951b1939c2544e7
Wagnerp <dev.wagnerp@outlook.com>
2018-04-11 19:02:00 +0100

    * Background work for a new application

---
+++ Source/Krypton Toolkit Suite Extended/Update File Creator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+using UpdateFileCreator.UI;
+
+namespace UpdateFileCreator
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new SplashForm());
+        }
+    }
+}
---
+++ Source/Krypton Toolkit Suite Extended/Update File Creator/UI/SplashForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace UpdateFileCreator.UI
+{
+    partial class SplashForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.kpnlBackground = new ComponentFactory.Krypton.Toolkit.KryptonPanel();
+            this.kMan = new ComponentFactory.Krypton.Toolkit.KryptonManager(this.components);
+            this.tmrLoad = new System.Windows.Forms.Timer(this.components);
+            this.pbLoad = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.kpnlBackground)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kpnlBackground
+            // 
+            this.kpnlBackground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.kpnlBackground.Location = new System.Drawing.Point(0, 0);
+            this.kpnlBackground.Name = "kpnlBackground";
+            this.kpnlBackground.Size = new System.Drawing.Size(800, 450);
+            this.kpnlBackground.TabIndex = 0;
+            // 
+            // tmrLoad
+            // 
+            this.tmrLoad.Enabled = true;
+            this.tmrLoad.Interval = 250;
+            // 
+            // pbLoad
+            // 
+            this.pbLoad.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pbLoad.Location = new System.Drawing.Point(0, 435);
+            this.pbLoad.Name = "pbLoad";
+            this.pbLoad.Size = new System.Drawing.Size(800, 15);
+            this.pbLoad.TabIndex = 1;
+            // 
+            // SplashForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.pbLoad);
+            this.Controls.Add(this.kpnlBackground);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SplashForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.kpnlBackground)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ComponentFactory.Krypton.Toolkit.KryptonPanel kpnlBackground;
+        private ComponentFactory.Krypton.Toolkit.KryptonManager kMan;
+        private System.Windows.Forms.Timer tmrLoad;
+        private System.Windows.Forms.ProgressBar pbLoad;
+    }
+}
