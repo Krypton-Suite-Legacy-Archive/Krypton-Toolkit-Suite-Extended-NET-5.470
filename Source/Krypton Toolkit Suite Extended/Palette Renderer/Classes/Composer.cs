5eee46189261de166bd530f5ce046d00545b7910
Peter Wagner <peterwagner@live.co.uk>
2018-05-23 10:21:52 +0100

    * Changes

--- Source/Krypton Toolkit Suite Extended/Palette Renderer/Classes/Composer.cs
+++ Source/Krypton Toolkit Suite Extended/Palette Renderer/Classes/Composer.cs
@@ -2,11 +2,75 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.CodeDom.Compiler;
+using System.IO;
+using Microsoft.CSharp;
 
 namespace PaletteRenderer.Classes
 {
-    class Composer
+    public class Composer
     {
+        #region Variables
+        private string _outputFile, _namespace;
+
+        private CSharpCodeProvider _provider;
+
+        private List<string> _sources, _paletteItems;
+
+        private CompilerParameters _compilerParameters;
+
+        private StringBuilder _managerBuilder;
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Initialises a new instance of the <see cref="Composer"/> class.
+        /// </summary>
+        /// <param name="outputFile">The output file.</param>
+        /// <param name="nameSpace">The name space.</param>
+        /// <param name="toolkitAssembly">The toolkit assembly.</param>
+        public Composer(string outputFile, string nameSpace, string toolkitAssembly)
+        {
+            _outputFile = outputFile;
+
+            _namespace = nameSpace;
+
+            _provider = new CSharpCodeProvider(new Dictionary<string, string>() { { "CompilerVersion", "v3.5" } });
+
+            _sources = new List<string>();
+
+            _paletteItems = new List<string>();
+
+            _compilerParameters = new CompilerParameters();
+
+            _compilerParameters.ReferencedAssemblies.Add("System.dll");
+
+            _compilerParameters.ReferencedAssemblies.Add("System.Core.dll");
+
+            _compilerParameters.ReferencedAssemblies.Add("System.Data.dll");
+
+            _compilerParameters.ReferencedAssemblies.Add("System.Xml.dll");
+
+            _compilerParameters.ReferencedAssemblies.Add("System.Xml.Linq.dll");
+
+            _compilerParameters.ReferencedAssemblies.Add("System.Data.DataSetExtensions.dll");
+
+            _compilerParameters.ReferencedAssemblies.Add(toolkitAssembly);
+
+            _compilerParameters.CompilerOptions = "";
+
+            _compilerParameters.GenerateExecutable = false;
+
+            _compilerParameters.GenerateInMemory = false;
+
+            _compilerParameters.OutputAssembly = _outputFile;
+
+            CreateManager();
+
+            AddPaletteItemsClass();
+
+            CreateAssemblyInfo();
+        }
+        #endregion
     }
 }
--- Source/Krypton Toolkit Suite Extended/Palette Renderer/Classes/Utilities.cs
+++ Source/Krypton Toolkit Suite Extended/Palette Renderer/Classes/Utilities.cs
@@ -2,16 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.CodeDom.Compiler;
-using System.IO;
-using Microsoft.CSharp;
 
 namespace PaletteRenderer.Classes
 {
     public class Utilities
     {
         #region Variables
-        private string _outputFile, 
+        
         #endregion
     }
 }
