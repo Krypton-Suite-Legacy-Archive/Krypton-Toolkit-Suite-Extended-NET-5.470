fd659286c042f3c3c20e694275460babe88c2904
Smurf-IV <smurf.iv@gmail.com>
2019-01-05 16:28:42 +0000

    * Add Code to the Playground to show defaults

---
+++ Source/Krypton Toolkit Suite Extended/Playground/CommandLinkStyles.cs
@@ -0,0 +1,12 @@
+﻿using ComponentFactory.Krypton.Toolkit;
+
+namespace Playground
+{
+    public partial class CommandLinkStyles : KryptonForm
+    {
+        public CommandLinkStyles()
+        {
+            InitializeComponent();
+        }
+    }
+}
