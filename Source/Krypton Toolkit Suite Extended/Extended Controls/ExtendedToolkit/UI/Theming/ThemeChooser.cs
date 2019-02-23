0e27723aa7efdcc3d842e33d24cc9d1af49a8145
Peter Wagner <peterwagner@live.co.uk>
2019-02-23 10:48:47 +0000

    * Improvements

---
+++ Source/Krypton Toolkit Suite Extended/Extended Controls/ExtendedToolkit/UI/Theming/ThemeChooser.cs
@@ -0,0 +1,195 @@
+﻿using ComponentFactory.Krypton.Toolkit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtendedControls.ExtendedToolkit.UI.Theming
+{
+    public class ThemeChooser : KryptonForm
+    {
+        private KryptonPanel kryptonPanel1;
+        private KryptonPanel kryptonPanel2;
+        private KryptonButton kbtnCreate;
+        private KryptonButton kbtnApply;
+        private KryptonButton kbtnCancel;
+        private KryptonButton kryptonButton1;
+        private KryptonButton kbtnBrowse;
+        private Controls.KryptonPromptTextBox kryptonPromptTextBox1;
+        private KryptonComboBox kcmbSelection;
+        private KryptonLabel klblPaletteStyle;
+        private System.Windows.Forms.Panel panel1;
+
+        private void InitializeComponent()
+        {
+            this.kryptonPanel1 = new ComponentFactory.Krypton.Toolkit.KryptonPanel();
+            this.kryptonPanel2 = new ComponentFactory.Krypton.Toolkit.KryptonPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.kbtnApply = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.kbtnCancel = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.kbtnCreate = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.kcmbSelection = new ComponentFactory.Krypton.Toolkit.KryptonComboBox();
+            this.klblPaletteStyle = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.kryptonPromptTextBox1 = new ExtendedControls.ExtendedToolkit.Controls.KryptonPromptTextBox();
+            this.kbtnBrowse = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.kryptonButton1 = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).BeginInit();
+            this.kryptonPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel2)).BeginInit();
+            this.kryptonPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kcmbSelection)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kryptonPanel1
+            // 
+            this.kryptonPanel1.Controls.Add(this.kryptonButton1);
+            this.kryptonPanel1.Controls.Add(this.kbtnBrowse);
+            this.kryptonPanel1.Controls.Add(this.kryptonPromptTextBox1);
+            this.kryptonPanel1.Controls.Add(this.kcmbSelection);
+            this.kryptonPanel1.Controls.Add(this.klblPaletteStyle);
+            this.kryptonPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.kryptonPanel1.Location = new System.Drawing.Point(0, 0);
+            this.kryptonPanel1.Name = "kryptonPanel1";
+            this.kryptonPanel1.Size = new System.Drawing.Size(702, 150);
+            this.kryptonPanel1.TabIndex = 0;
+            // 
+            // kryptonPanel2
+            // 
+            this.kryptonPanel2.Controls.Add(this.kbtnCreate);
+            this.kryptonPanel2.Controls.Add(this.kbtnApply);
+            this.kryptonPanel2.Controls.Add(this.kbtnCancel);
+            this.kryptonPanel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.kryptonPanel2.Location = new System.Drawing.Point(0, 152);
+            this.kryptonPanel2.Name = "kryptonPanel2";
+            this.kryptonPanel2.Size = new System.Drawing.Size(702, 52);
+            this.kryptonPanel2.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 150);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(702, 54);
+            this.panel1.TabIndex = 0;
+            // 
+            // kbtnApply
+            // 
+            this.kbtnApply.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.kbtnApply.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.kbtnApply.Enabled = false;
+            this.kbtnApply.Location = new System.Drawing.Point(450, 11);
+            this.kbtnApply.Name = "kbtnApply";
+            this.kbtnApply.Size = new System.Drawing.Size(117, 29);
+            this.kbtnApply.StateCommon.Content.LongText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kbtnApply.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kbtnApply.TabIndex = 12;
+            this.kbtnApply.Values.Text = "A&pply";
+            // 
+            // kbtnCancel
+            // 
+            this.kbtnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.kbtnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.kbtnCancel.Location = new System.Drawing.Point(573, 11);
+            this.kbtnCancel.Name = "kbtnCancel";
+            this.kbtnCancel.Size = new System.Drawing.Size(117, 29);
+            this.kbtnCancel.StateCommon.Content.LongText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kbtnCancel.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kbtnCancel.TabIndex = 11;
+            this.kbtnCancel.Values.Text = "&Cancel";
+            // 
+            // kbtnCreate
+            // 
+            this.kbtnCreate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.kbtnCreate.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.kbtnCreate.Enabled = false;
+            this.kbtnCreate.Location = new System.Drawing.Point(12, 11);
+            this.kbtnCreate.Name = "kbtnCreate";
+            this.kbtnCreate.Size = new System.Drawing.Size(180, 29);
+            this.kbtnCreate.StateCommon.Content.LongText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kbtnCreate.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kbtnCreate.TabIndex = 13;
+            this.kbtnCreate.Values.Text = "Create &Custom Theme";
+            // 
+            // kcmbSelection
+            // 
+            this.kcmbSelection.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.kcmbSelection.DropDownWidth = 255;
+            this.kcmbSelection.Location = new System.Drawing.Point(172, 12);
+            this.kcmbSelection.Name = "kcmbSelection";
+            this.kcmbSelection.Size = new System.Drawing.Size(518, 25);
+            this.kcmbSelection.StateCommon.ComboBox.Content.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kcmbSelection.StateCommon.Item.Content.LongText.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kcmbSelection.StateCommon.Item.Content.ShortText.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kcmbSelection.TabIndex = 5;
+            // 
+            // klblPaletteStyle
+            // 
+            this.klblPaletteStyle.Location = new System.Drawing.Point(12, 12);
+            this.klblPaletteStyle.Name = "klblPaletteStyle";
+            this.klblPaletteStyle.Size = new System.Drawing.Size(154, 24);
+            this.klblPaletteStyle.StateCommon.LongText.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.klblPaletteStyle.StateCommon.ShortText.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.klblPaletteStyle.TabIndex = 4;
+            this.klblPaletteStyle.Values.Text = "Select Palette Style:";
+            // 
+            // kryptonPromptTextBox1
+            // 
+            this.kryptonPromptTextBox1.DrawPrompt = true;
+            this.kryptonPromptTextBox1.FocusSelect = true;
+            this.kryptonPromptTextBox1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kryptonPromptTextBox1.Location = new System.Drawing.Point(26, 60);
+            this.kryptonPromptTextBox1.Name = "kryptonPromptTextBox1";
+            this.kryptonPromptTextBox1.PromptForeColour = System.Drawing.SystemColors.GrayText;
+            this.kryptonPromptTextBox1.PromptText = "Enter custom theme path here...";
+            this.kryptonPromptTextBox1.PromptTypeface = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kryptonPromptTextBox1.Size = new System.Drawing.Size(627, 27);
+            this.kryptonPromptTextBox1.TabIndex = 6;
+            // 
+            // kbtnBrowse
+            // 
+            this.kbtnBrowse.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.kbtnBrowse.Location = new System.Drawing.Point(659, 60);
+            this.kbtnBrowse.Name = "kbtnBrowse";
+            this.kbtnBrowse.Size = new System.Drawing.Size(31, 27);
+            this.kbtnBrowse.StateCommon.Content.LongText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kbtnBrowse.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kbtnBrowse.TabIndex = 12;
+            this.kbtnBrowse.Values.Text = ".&..";
+            // 
+            // kryptonButton1
+            // 
+            this.kryptonButton1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.kryptonButton1.Location = new System.Drawing.Point(573, 104);
+            this.kryptonButton1.Name = "kryptonButton1";
+            this.kryptonButton1.Size = new System.Drawing.Size(117, 29);
+            this.kryptonButton1.StateCommon.Content.LongText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kryptonButton1.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kryptonButton1.TabIndex = 13;
+            this.kryptonButton1.Values.Text = "&Cancel";
+            // 
+            // ThemeChooser
+            // 
+            this.ClientSize = new System.Drawing.Size(702, 204);
+            this.Controls.Add(this.kryptonPanel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.kryptonPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ThemeChooser";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Choose a Theme";
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).EndInit();
+            this.kryptonPanel1.ResumeLayout(false);
+            this.kryptonPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel2)).EndInit();
+            this.kryptonPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.kcmbSelection)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+    }
+}
