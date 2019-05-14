2884e6e93d617208ba142f3498d3b88aed2772b7
Peter Wagner <peterwagner@live.co.uk>
2019-05-14 16:17:47 +0100

    * Code

---
+++ Source/Krypton Toolkit Suite Extended/Palette Explorer/UX/Colour/ColourMixerAdvanced.cs
@@ -0,0 +1,34 @@
+﻿using ComponentFactory.Krypton.Toolkit;
+using System;
+
+namespace PaletteExplorer.UX.Colour
+{
+    public class ColourMixerAdvanced : KryptonForm
+    {
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ColourMixerAdvanced
+            // 
+            this.ClientSize = new System.Drawing.Size(1400, 849);
+            this.Name = "ColourMixerAdvanced";
+            this.Load += new System.EventHandler(this.ColourMixerAdvanced_Load);
+            this.ResumeLayout(false);
+
+        }
+
+
+        #region Constructors
+        public ColourMixerAdvanced()
+        {
+
+        }
+        #endregion
+
+        private void ColourMixerAdvanced_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
