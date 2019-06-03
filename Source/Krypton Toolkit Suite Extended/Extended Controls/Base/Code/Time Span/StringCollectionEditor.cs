80b3a5e52e3a9ac0a2b532d8ac4d5db36c440895
Peter Wagner <peterwagner@live.co.uk>
2019-06-03 18:42:25 +0100

    * Code

--- Source/Krypton Toolkit Suite Extended/Extended Controls/Base/Code/Time Span/StringCollectionEditor.cs
+++ Source/Krypton Toolkit Suite Extended/Extended Controls/Base/Code/Time Span/StringCollectionEditor.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ComponentFactory.Krypton.Toolkit;
+using System;
+using System.ComponentModel;
 using System.ComponentModel.Design;
+using System.Windows.Forms;
 
 namespace ExtendedControls.Base.Code.TimeSpan
 {
@@ -9,8 +12,208 @@ namespace ExtendedControls.Base.Code.TimeSpan
 
         protected virtual string FormTitle => null;
 
+        protected override CollectionForm CreateCollectionForm() => new StringCollectionForm(this);
+
         public StringCollectionEditor(Type type) : base(type)
         {
+        }
+
+        private class StringCollectionForm : CollectionForm
+        {
+            #region Variables
+            private KryptonButton _kbtnCancel, _kbtnOk;
+
+            private readonly StringCollectionEditor _editor;
+
+            private KryptonLabel _klblInstruction;
+
+            private TableLayoutPanel _tlpOkCancelTableLayoutPanel;
+
+            private KryptonTextBox _ktxtEntry;
+            #endregion
+
+            #region Designer Code
+            private void InitializeComponent()
+            {
+                var manager = new ComponentResourceManager(Type.GetType("System.Windows.Forms.Design.StringCollectionEditor, System.Design"));
+                _klblInstruction = new KryptonLabel();
+                _ktxtEntry = new KryptonTextBox();
+                _kbtnOk = new KryptonButton();
+                _kbtnCancel = new KryptonButton();
+                _tlpOkCancelTableLayoutPanel = new TableLayoutPanel();
+                _tlpOkCancelTableLayoutPanel.SuspendLayout();
+                SuspendLayout();
+                manager.ApplyResources(_klblInstruction, "klblInstruction");
+                _klblInstruction.Margin = new Padding(3, 1, 3, 0);
+                _klblInstruction.Name = "klblInstruction";
+                if (_editor.InstructionText != null)
+                    _klblInstruction.Text = _editor.InstructionText;
+                _ktxtEntry.AcceptsTab = true;
+                _ktxtEntry.AcceptsReturn = true;
+                manager.ApplyResources(_ktxtEntry, "ktxtEntry");
+                _ktxtEntry.Name = "ktxtEntry";
+                manager.ApplyResources(_kbtnOk, "okButton");
+                _kbtnOk.DialogResult = DialogResult.OK;
+                _kbtnOk.Margin = new Padding(0, 0, 3, 0);
+                _kbtnOk.Name = "okButton";
+                manager.ApplyResources(_kbtnCancel, "cancelButton");
+                _kbtnCancel.DialogResult = DialogResult.Cancel;
+                _kbtnCancel.Margin = new Padding(3, 0, 0, 0);
+                _kbtnCancel.Name = "cancelButton";
+                manager.ApplyResources(_tlpOkCancelTableLayoutPanel, "tlpOkCancelTableLayoutPanel");
+                _tlpOkCancelTableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50f));
+                _tlpOkCancelTableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50f));
+                _tlpOkCancelTableLayoutPanel.Controls.Add(_kbtnOk, 0, 0);
+                _tlpOkCancelTableLayoutPanel.Controls.Add(_kbtnCancel, 1, 0);
+                _tlpOkCancelTableLayoutPanel.Name = "tlpOkCancelTableLayoutPanel";
+                _tlpOkCancelTableLayoutPanel.RowStyles.Add(new RowStyle());
+                manager.ApplyResources(this, "$this");
+                AutoScaleMode = AutoScaleMode.Font;
+                Controls.Add(_tlpOkCancelTableLayoutPanel);
+                Controls.Add(_klblInstruction);
+                Controls.Add(_ktxtEntry);
+                HelpButton = true;
+                MaximizeBox = false;
+                MinimizeBox = false;
+                Name = "StringCollectionEditor";
+                ShowIcon = false;
+                ShowInTaskbar = false;
+                if (_editor.FormTitle != null)
+                    Text = _editor.FormTitle;
+                _tlpOkCancelTableLayoutPanel.ResumeLayout(false);
+                _tlpOkCancelTableLayoutPanel.PerformLayout();
+                HelpRequested += StringCollectionForm_HelpRequested;
+                ResumeLayout(false);
+                PerformLayout();
+            }
+            #endregion
+
+            #region Constructor
+            public StringCollectionForm(CollectionEditor editor) : base(editor)
+            {
+                _editor = (StringCollectionEditor)editor;
+
+                InitializeComponent();
+
+                HookEvents();
+            }
+            #endregion
+
+            private void HookEvents()
+            {
+                _ktxtEntry.KeyDown += Entry_KeyDown;
+
+                _kbtnOk.Click += Ok_Click;
+
+                HelpButtonClicked += StringCollectionForm_HelpButtonClicked;
+            }
+
+            private void StringCollectionForm_HelpButtonClicked(object sender, CancelEventArgs e)
+            {
+                e.Cancel = true;
+
+                _editor.ShowHelp();
+            }
+
+            private void Ok_Click(object sender, EventArgs e)
+            {
+                var separator = new[] { '\n' };
+
+                var trimChars = new[] { '\r' };
+
+                var stringArray = _ktxtEntry.Text.Split(separator);
+
+                var items = Items;
+
+                var length = stringArray.Length;
+
+                for (var i = 0; i < length; i++)
+                {
+                    stringArray[i] = stringArray[i].Trim(trimChars);
+                }
+
+                var flag = true;
+
+                if (length == items.Length)
+                {
+                    var index = 0;
+
+                    while (index < length)
+                    {
+                        if (!stringArray[index].Equals((string)items[index]))
+                        {
+                            break;
+                        }
+
+                        index++;
+                    }
+
+                    if (index == length)
+                    {
+                        flag = false;
+                    }
+                }
+
+                if (!flag)
+                {
+                    DialogResult = DialogResult.Cancel;
+                }
+                else
+                {
+                    if (stringArray.Length > 0 && stringArray[stringArray.Length - 1].Length == 0)
+                    {
+                        length--;
+                    }
+
+                    var objectArray = new object[length];
+
+                    for (var j = 0; j < length; j++)
+                    {
+                        objectArray[j] = stringArray[j];
+                    }
+
+                    Items = objectArray;
+                }
+            }
+
+            private void Entry_KeyDown(object sender, KeyEventArgs e)
+            {
+                if (e.KeyCode == Keys.Escape)
+                {
+                    _kbtnCancel.PerformClick();
+
+                    e.Handled = true;
+                }
+            }
+
+            protected override void OnEditValueChanged()
+            {
+                var items = Items;
+
+                var str = string.Empty;
+
+                for (var i = 0; i < items.Length; i++)
+                {
+                    var s = items[i] as string;
+
+                    if (s != null)
+                    {
+                        str = str + s;
+
+                        if (i != items.Length - 1)
+                        {
+                            str = str + "\r\n";
+                        }
+                    }
+                }
+
+                _ktxtEntry.Text = str;
+            }
+
+            private void StringCollectionForm_HelpRequested(object sender, HelpEventArgs hlpevent)
+            {
+                _editor.ShowHelp();
+            }
         }
     }
 }
---
+++ Source/Krypton Toolkit Suite Extended/Extended Controls/Base/Code/Time Span/StrongListWrapper.cs
@@ -0,0 +1,270 @@
+﻿namespace System.Collections.Generic
+{
+    /// <summary>
+    /// Takes a collection that supports an <see cref="IList"/> and makes it strongly typed like <see cref="List{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">Type to cast object to from the provided <see cref="IList"/>.</typeparam>
+    public class StrongListWrapper<T> : IList<T>, ICollection
+    {
+        private readonly IList list;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StrongListWrapper&lt;T&gt;"/> class.
+        /// </summary>
+        /// <param name="baseList">The base list.</param>
+        public StrongListWrapper(IList baseList)
+        {
+            list = baseList;
+        }
+
+        /// <summary>
+        /// Gets the core list.
+        /// </summary>
+        /// <value>The core list.</value>
+        public IList Core => list;
+
+        /// <summary>
+        /// Gets the number of elements contained in the <see cref="T:System.Collections.Generic.ICollection`1"/>.
+        /// </summary>
+        /// <value></value>
+        /// <returns>
+        /// The number of elements contained in the <see cref="T:System.Collections.Generic.ICollection`1"/>.
+        /// </returns>
+        public int Count => list.Count;
+
+        /// <summary>
+        /// Gets a value indicating whether the <see cref="T:System.Collections.Generic.ICollection`1"/> is read-only.
+        /// </summary>
+        /// <value></value>
+        /// <returns>true if the <see cref="T:System.Collections.Generic.ICollection`1"/> is read-only; otherwise, false.
+        /// </returns>
+        public bool IsReadOnly => list.IsReadOnly;
+
+        int ICollection.Count => list.Count;
+
+        bool ICollection.IsSynchronized => list.IsSynchronized;
+
+        object ICollection.SyncRoot => list.SyncRoot;
+
+        /// <summary>
+        /// Gets or sets the strongly typed object at the specified index.
+        /// </summary>
+        /// <value>The index of the list item to get.</value>
+        public T this[int index]
+        {
+            get
+            {
+                return (T)list[index];
+            }
+            set
+            {
+                list[index] = value;
+            }
+        }
+
+        /// <summary>
+        /// Adds an item to the <see cref="T:System.Collections.Generic.ICollection`1"/>.
+        /// </summary>
+        /// <param name="item">The object to add to the <see cref="T:System.Collections.Generic.ICollection`1"/>.</param>
+        /// <exception cref="T:System.NotSupportedException">
+        /// The <see cref="T:System.Collections.Generic.ICollection`1"/> is read-only.
+        /// </exception>
+        public void Add(T item)
+        {
+            list.Add(item);
+        }
+
+        /// <summary>
+        /// Adds the range.
+        /// </summary>
+        /// <param name="items">The items.</param>
+        public void AddRange(T[] items)
+        {
+            if (items == null)
+                throw new ArgumentNullException();
+            for (var i = 0; i < items.Length; i++)
+                list.Add(items[i]);
+        }
+
+        /// <summary>
+        /// Removes all items from the <see cref="T:System.Collections.Generic.ICollection`1"/>.
+        /// </summary>
+        /// <exception cref="T:System.NotSupportedException">
+        /// The <see cref="T:System.Collections.Generic.ICollection`1"/> is read-only.
+        /// </exception>
+        public void Clear()
+        {
+            list.Clear();
+        }
+
+        /// <summary>
+        /// Determines whether the <see cref="T:System.Collections.Generic.ICollection`1"/> contains a specific value.
+        /// </summary>
+        /// <param name="item">The object to locate in the <see cref="T:System.Collections.Generic.ICollection`1"/>.</param>
+        /// <returns>
+        /// true if <paramref name="item"/> is found in the <see cref="T:System.Collections.Generic.ICollection`1"/>; otherwise, false.
+        /// </returns>
+        public bool Contains(T item) => list.Contains(item);
+
+        /// <summary>
+        /// Copies the elements of the <see cref="T:System.Collections.Generic.ICollection`1"/> to an <see cref="T:System.Array"/>, starting at a particular <see cref="T:System.Array"/> index.
+        /// </summary>
+        /// <param name="array">The one-dimensional <see cref="T:System.Array"/> that is the destination of the elements copied from <see cref="T:System.Collections.Generic.ICollection`1"/>. The <see cref="T:System.Array"/> must have zero-based indexing.</param>
+        /// <param name="arrayIndex">The zero-based index in <paramref name="array"/> at which copying begins.</param>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// 	<paramref name="array"/> is null.
+        /// </exception>
+        /// <exception cref="T:System.ArgumentOutOfRangeException">
+        /// 	<paramref name="arrayIndex"/> is less than 0.
+        /// </exception>
+        /// <exception cref="T:System.ArgumentException">
+        /// 	<paramref name="array"/> is multidimensional.
+        /// -or-
+        /// <paramref name="arrayIndex"/> is equal to or greater than the length of <paramref name="array"/>.
+        /// -or-
+        /// The number of elements in the source <see cref="T:System.Collections.Generic.ICollection`1"/> is greater than the available space from <paramref name="arrayIndex"/> to the end of the destination <paramref name="array"/>.
+        /// -or-
+        /// Type T cannot be cast automatically to the type of the destination <paramref name="array"/>.
+        /// </exception>
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            list.CopyTo(array, arrayIndex);
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="T:System.Collections.Generic.IEnumerator`1"/> that can be used to iterate through the collection.
+        /// </returns>
+        public IEnumerator<T> GetEnumerator() => new StrongListWrapperEnumerator<T>(list.GetEnumerator());
+
+        void ICollection.CopyTo(Array array, int index)
+        {
+            list.CopyTo(array, index);
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through a collection.
+        /// </summary>
+        /// <returns>
+        /// An <see cref="T:System.Collections.IEnumerator"/> object that can be used to iterate through the collection.
+        /// </returns>
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        /// <summary>
+        /// Determines the index of a specific item in the <see cref="T:System.Collections.Generic.IList`1"/>.
+        /// </summary>
+        /// <param name="item">The object to locate in the <see cref="T:System.Collections.Generic.IList`1"/>.</param>
+        /// <returns>
+        /// The index of <paramref name="item"/> if found in the list; otherwise, -1.
+        /// </returns>
+        public int IndexOf(T item) => list.IndexOf(item);
+
+        /// <summary>
+        /// Inserts an item to the <see cref="T:System.Collections.Generic.IList`1"/> at the specified index.
+        /// </summary>
+        /// <param name="index">The zero-based index at which <paramref name="item"/> should be inserted.</param>
+        /// <param name="item">The object to insert into the <see cref="T:System.Collections.Generic.IList`1"/>.</param>
+        /// <exception cref="T:System.ArgumentOutOfRangeException">
+        /// 	<paramref name="index"/> is not a valid index in the <see cref="T:System.Collections.Generic.IList`1"/>.
+        /// </exception>
+        /// <exception cref="T:System.NotSupportedException">
+        /// The <see cref="T:System.Collections.Generic.IList`1"/> is read-only.
+        /// </exception>
+        public void Insert(int index, T item)
+        {
+            list.Insert(index, item);
+        }
+
+        /// <summary>
+        /// Removes the first occurrence of a specific object from the <see cref="T:System.Collections.Generic.ICollection`1"/>.
+        /// </summary>
+        /// <param name="item">The object to remove from the <see cref="T:System.Collections.Generic.ICollection`1"/>.</param>
+        /// <returns>
+        /// true if <paramref name="item"/> was successfully removed from the <see cref="T:System.Collections.Generic.ICollection`1"/>; otherwise, false. This method also returns false if <paramref name="item"/> is not found in the original <see cref="T:System.Collections.Generic.ICollection`1"/>.
+        /// </returns>
+        /// <exception cref="T:System.NotSupportedException">
+        /// The <see cref="T:System.Collections.Generic.ICollection`1"/> is read-only.
+        /// </exception>
+        public bool Remove(T item) => Remove(item);
+
+        /// <summary>
+        /// Removes the <see cref="T:System.Collections.Generic.IList`1"/> item at the specified index.
+        /// </summary>
+        /// <param name="index">The zero-based index of the item to remove.</param>
+        /// <exception cref="T:System.ArgumentOutOfRangeException">
+        /// 	<paramref name="index"/> is not a valid index in the <see cref="T:System.Collections.Generic.IList`1"/>.
+        /// </exception>
+        /// <exception cref="T:System.NotSupportedException">
+        /// The <see cref="T:System.Collections.Generic.IList`1"/> is read-only.
+        /// </exception>
+        public void RemoveAt(int index)
+        {
+            list.RemoveAt(index);
+        }
+
+        /// <summary>
+        /// Enumerator class for the <see cref="StrongListWrapper{T}"/>.
+        /// </summary>
+        /// <typeparam name="ET">Type to cast object to from the provided <see cref="IEnumerator"/>.</typeparam>
+        public class StrongListWrapperEnumerator<ET> : IEnumerator<ET>
+        {
+            private IEnumerator iEnum;
+
+            internal StrongListWrapperEnumerator(IEnumerator ienum)
+            {
+                iEnum = ienum;
+            }
+
+            /// <summary>
+            /// Gets the element in the collection at the current position of the enumerator.
+            /// </summary>
+            /// <value></value>
+            /// <returns>
+            /// The element in the collection at the current position of the enumerator.
+            /// </returns>
+            public ET Current => (ET)iEnum.Current;
+
+            /// <summary>
+            /// Gets the element in the collection at the current position of the enumerator.
+            /// </summary>
+            /// <value></value>
+            /// <returns>
+            /// The element in the collection at the current position of the enumerator.
+            /// </returns>
+            object IEnumerator.Current => Current;
+
+            /// <summary>
+            /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+            /// </summary>
+            public void Dispose()
+            {
+                Reset();
+                iEnum = null;
+            }
+
+            /// <summary>
+            /// Advances the enumerator to the next element of the collection.
+            /// </summary>
+            /// <returns>
+            /// true if the enumerator was successfully advanced to the next element; false if the enumerator has passed the end of the collection.
+            /// </returns>
+            /// <exception cref="T:System.InvalidOperationException">
+            /// The collection was modified after the enumerator was created.
+            /// </exception>
+            public bool MoveNext() => iEnum.MoveNext();
+
+            /// <summary>
+            /// Sets the enumerator to its initial position, which is before the first element in the collection.
+            /// </summary>
+            /// <exception cref="T:System.InvalidOperationException">
+            /// The collection was modified after the enumerator was created.
+            /// </exception>
+            public void Reset()
+            {
+                iEnum.Reset();
+            }
+        }
+    }
+}
