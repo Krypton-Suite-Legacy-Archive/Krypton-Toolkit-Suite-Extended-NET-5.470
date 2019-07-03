c1b02812d705dc2da5d954c17886a846364018cd
Peter Wagner <peterwagner@live.co.uk>
2019-07-03 10:59:52 +0100

    Create KryptonColourButtonExtendedCustomColourUI.cs

---
+++ Source/Krypton Toolkit Suite Extended/Extended Controls/ExtendedToolkit/UI/Colours/KryptonColourButtonExtendedCustomColourUI.cs
@@ -0,0 +1,8 @@
+﻿using ComponentFactory.Krypton.Toolkit;
+
+namespace ExtendedControls.ExtendedToolkit.UI.Colours
+{
+    internal class KryptonColourButtonExtendedCustomColourUI : KryptonForm
+    {
+    }
+}
