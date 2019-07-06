fe5989f81613514ee88b49071c3d46c5b6da995f
Peter Wagner <peterwagner@live.co.uk>
2019-07-06 14:04:19 +0100

    * Changes

---
+++ Source/Krypton Toolkit Suite Extended/Core/Components/KryptonMessageBoxExtendedComoponent.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace KryptonToolkitSuiteExtendedCore
+{
+    public class KryptonMessageBoxExtendedComoponent : Component
+    {
+        #region Variables
+        private bool? _showCtrlCopy;
+
+        private bool _showDoNotShowAgainOption, _useTimeOutOption;
+
+        Font _messageboxTypeface;
+
+        DialogResult _defaultTimeOutResponse;
+
+        MessageBoxButtons _buttons;
+
+        MessageBoxDefaultButton _defaultButton;
+
+        MessageBoxIcon _icon;
+
+        MessageBoxOptions _options;
+
+        KryptonMessageBoxExtended.HelpInformation _helpInformation;
+
+        IWin32Window _owner;
+        #endregion
+
+        #region Properties
+        public bool? ShowControlCopy { get => _showCtrlCopy; set => _showCtrlCopy = value; }
+        #endregion
+
+        #region Event
+        //public event ShowMessageBox;
+        #endregion
+    }
+}
