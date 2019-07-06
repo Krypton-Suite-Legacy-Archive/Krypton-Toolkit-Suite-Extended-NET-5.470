d9d972fc82c5af0f03f3664aceb3747a8786ae1a
Peter Wagner <peterwagner@live.co.uk>
2019-07-06 19:10:27 +0100

    * Code

--- Source/Krypton Toolkit Suite Extended/Extended Colour Controls/Controls/Numeric Boxes/AlphaValueNumericBox.cs
+++ Source/Krypton Toolkit Suite Extended/Extended Colour Controls/Controls/Numeric Boxes/AlphaValueNumericBox.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ComponentFactory.Krypton.Toolkit;
+using System.Drawing;
 
-namespace ExtendedColourControls.Controls.Numeric_Boxes
+namespace ExtendedColourControls
 {
-    class AlphaValueNumericBox
+    public class AlphaValueNumericBox : KryptonNumericUpDown
     {
+        #region Constants
+        private const int DEFAULT_VALUE = 0, MINIMUM_DEFAULT_VALUE = 0, MAXIMUM_DEFAULT_VALUE = 255;
+
+        private Color DEFAULT_BACK_COLOUR = Color.White, DEFAULT_FORECOLOUR = Color.Black;
+
+        private Font DEFAULT_TYPEFACE = new Font("Segoe UI", 11f);
+        #endregion
+
+        #region Constructor
+        public AlphaValueNumericBox()
+        {
+            Minimum = MINIMUM_DEFAULT_VALUE;
+
+            Maximum = MAXIMUM_DEFAULT_VALUE;
+
+            Value = DEFAULT_VALUE;
+
+            StateCommon.Back.Color1 = DEFAULT_BACK_COLOUR;
+
+            StateCommon.Content.Color1 = DEFAULT_FORECOLOUR;
+
+            StateCommon.Content.Font = DEFAULT_TYPEFACE;
+        }
+        #endregion
     }
 }
--- Source/Krypton Toolkit Suite Extended/Playground/Componentisation/Colours/ColourControls.cs
+++ Source/Krypton Toolkit Suite Extended/Playground/Componentisation/Colours/ColourControls.cs
@@ -9,31 +9,88 @@ namespace Playground.Componentisation.Colours
 {
     public class ColourControls : KryptonForm
     {
-        private ExtendedColourControls.RedValueNumericBox redNumericBox1;
+        private ExtendedColourControls.AlphaValueNumericBox alphaValueNumericBox1;
+        private ExtendedColourControls.BlueValueNumericBox blueValueNumericBox1;
+        private ExtendedColourControls.GreenValueNumericBox greenValueNumericBox1;
+        private ExtendedColourControls.RedValueNumericBox redValueNumericBox1;
         private ExtendedColourControls.ColourWheel colourWheel1;
 
         private void InitializeComponent()
         {
-            this.redNumericBox1 = new ExtendedColourControls.RedValueNumericBox();
+            this.redValueNumericBox1 = new ExtendedColourControls.RedValueNumericBox();
+            this.greenValueNumericBox1 = new ExtendedColourControls.GreenValueNumericBox();
+            this.blueValueNumericBox1 = new ExtendedColourControls.BlueValueNumericBox();
+            this.alphaValueNumericBox1 = new ExtendedColourControls.AlphaValueNumericBox();
             this.colourWheel1 = new ExtendedColourControls.ColourWheel();
             this.SuspendLayout();
             // 
-            // redNumericBox1
+            // redValueNumericBox1
             // 
-            this.redNumericBox1.DecimalPlaces = 99;
-            this.redNumericBox1.Location = new System.Drawing.Point(198, 52);
-            this.redNumericBox1.Maximum = new decimal(new int[] {
+            this.redValueNumericBox1.DecimalPlaces = 99;
+            this.redValueNumericBox1.Location = new System.Drawing.Point(202, 70);
+            this.redValueNumericBox1.Maximum = new decimal(new int[] {
             255,
             0,
             0,
             0});
-            this.redNumericBox1.Name = "redNumericBox1";
-            this.redNumericBox1.Size = new System.Drawing.Size(120, 26);
-            this.redNumericBox1.StateCommon.Back.Color1 = System.Drawing.Color.Red;
-            this.redNumericBox1.StateCommon.Content.Color1 = System.Drawing.Color.White;
-            this.redNumericBox1.StateCommon.Content.Font = new System.Drawing.Font("Segoe UI", 11F);
-            this.redNumericBox1.StateCommon.Content.TextH = ComponentFactory.Krypton.Toolkit.PaletteRelativeAlign.Inherit;
-            this.redNumericBox1.TabIndex = 0;
+            this.redValueNumericBox1.Name = "redValueNumericBox1";
+            this.redValueNumericBox1.Size = new System.Drawing.Size(120, 26);
+            this.redValueNumericBox1.StateCommon.Back.Color1 = System.Drawing.Color.Red;
+            this.redValueNumericBox1.StateCommon.Content.Color1 = System.Drawing.Color.White;
+            this.redValueNumericBox1.StateCommon.Content.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.redValueNumericBox1.StateCommon.Content.TextH = ComponentFactory.Krypton.Toolkit.PaletteRelativeAlign.Inherit;
+            this.redValueNumericBox1.TabIndex = 5;
+            // 
+            // greenValueNumericBox1
+            // 
+            this.greenValueNumericBox1.DecimalPlaces = 99;
+            this.greenValueNumericBox1.Location = new System.Drawing.Point(202, 101);
+            this.greenValueNumericBox1.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.greenValueNumericBox1.Name = "greenValueNumericBox1";
+            this.greenValueNumericBox1.Size = new System.Drawing.Size(120, 26);
+            this.greenValueNumericBox1.StateCommon.Back.Color1 = System.Drawing.Color.Green;
+            this.greenValueNumericBox1.StateCommon.Content.Color1 = System.Drawing.Color.White;
+            this.greenValueNumericBox1.StateCommon.Content.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.greenValueNumericBox1.StateCommon.Content.TextH = ComponentFactory.Krypton.Toolkit.PaletteRelativeAlign.Inherit;
+            this.greenValueNumericBox1.TabIndex = 4;
+            // 
+            // blueValueNumericBox1
+            // 
+            this.blueValueNumericBox1.DecimalPlaces = 99;
+            this.blueValueNumericBox1.Location = new System.Drawing.Point(202, 133);
+            this.blueValueNumericBox1.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.blueValueNumericBox1.Name = "blueValueNumericBox1";
+            this.blueValueNumericBox1.Size = new System.Drawing.Size(120, 26);
+            this.blueValueNumericBox1.StateCommon.Back.Color1 = System.Drawing.Color.Blue;
+            this.blueValueNumericBox1.StateCommon.Content.Color1 = System.Drawing.Color.White;
+            this.blueValueNumericBox1.StateCommon.Content.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.blueValueNumericBox1.StateCommon.Content.TextH = ComponentFactory.Krypton.Toolkit.PaletteRelativeAlign.Inherit;
+            this.blueValueNumericBox1.TabIndex = 3;
+            // 
+            // alphaValueNumericBox1
+            // 
+            this.alphaValueNumericBox1.DecimalPlaces = 99;
+            this.alphaValueNumericBox1.Location = new System.Drawing.Point(202, 38);
+            this.alphaValueNumericBox1.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.alphaValueNumericBox1.Name = "alphaValueNumericBox1";
+            this.alphaValueNumericBox1.Size = new System.Drawing.Size(120, 26);
+            this.alphaValueNumericBox1.StateCommon.Back.Color1 = System.Drawing.Color.White;
+            this.alphaValueNumericBox1.StateCommon.Content.Color1 = System.Drawing.Color.Black;
+            this.alphaValueNumericBox1.StateCommon.Content.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.alphaValueNumericBox1.StateCommon.Content.TextH = ComponentFactory.Krypton.Toolkit.PaletteRelativeAlign.Inherit;
+            this.alphaValueNumericBox1.TabIndex = 2;
             // 
             // colourWheel1
             // 
@@ -46,8 +103,11 @@ namespace Playground.Componentisation.Colours
             // ColourControls
             // 
             this.ClientSize = new System.Drawing.Size(1429, 725);
+            this.Controls.Add(this.redValueNumericBox1);
+            this.Controls.Add(this.greenValueNumericBox1);
+            this.Controls.Add(this.blueValueNumericBox1);
+            this.Controls.Add(this.alphaValueNumericBox1);
             this.Controls.Add(this.colourWheel1);
-            this.Controls.Add(this.redNumericBox1);
             this.Name = "ColourControls";
             this.ResumeLayout(false);
 
