4b0e7cf3619e722ef41b40757a6cb65f773da256
Peter Wagner <peterwagner@live.co.uk>
2019-07-06 16:03:35 +0100

    * Files

---
+++ Source/Krypton Toolkit Suite Extended/Extended Menu and Toolbar Items/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extended_Menu_and_Toolbar_Items
+{
+    public class Class1
+    {
+    }
+}
