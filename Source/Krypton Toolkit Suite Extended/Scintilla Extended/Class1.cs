ca2a2fbfae30eba7d57721111ff6b20cf2f722b2
Peter Wagner <peterwagner@live.co.uk>
2019-07-06 13:22:29 +0100

    * More #87

---
+++ Source/Krypton Toolkit Suite Extended/Scintilla Extended/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scintilla_Extended
+{
+    public class Class1
+    {
+    }
+}
