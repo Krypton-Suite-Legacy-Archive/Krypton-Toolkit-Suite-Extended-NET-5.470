d9d74dfeb9024be8285740ce25fad4573057e38b
Peter Wagner <peterwagner@live.co.uk>
2019-07-24 09:28:52 +0100

    * Controls

---
+++ Source/Krypton Toolkit Suite Extended/Extended Colour Controls/Controls/Trackbars/KryptonRedTrackBar.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using ComponentFactory.Krypton.Toolkit;
+
+namespace ExtendedColourControls
+{
+    [ToolboxBitmap(typeof(KryptonTrackBar))]
+    public class KryptonRedTrackBar : KryptonTrackBar
+    {
+        #region Constructor
+        public KryptonRedTrackBar()
+        {
+            Maximum = 255;
+
+            StateCommon.Tick.Color1 = Color.Red;
+
+            StateCommon.Track.Color1 = Color.Red;
+
+            StateCommon.Track.Color2 = Color.Red;
+
+            StateCommon.Track.Color3 = Color.Red;
+
+            StateCommon.Track.Color4 = Color.Red;
+
+            StateCommon.Track.Color5 = Color.Red;
+
+            TickStyle = TickStyle.None;
+
+            Size = new Size(182, 27);
+        }
+        #endregion
+    }
+}
---
+++ Source/Krypton Toolkit Suite Extended/Extended Dialogs/Dialogs/Colour/Palettes/KryptonPaletteColourMixerBasic.cs
@@ -0,0 +1,199 @@
+﻿using ComponentFactory.Krypton.Toolkit;
+
+namespace ExtendedDialogs
+{
+    public class KryptonPaletteColourMixerBasic : KryptonForm
+    {
+        private KryptonPanel kryptonPanel2;
+        private KryptonButton kbtnOptions;
+        private KryptonColorButton kcbBaseColour;
+        private KryptonButton kbtnDefineOtherColours;
+        private KryptonButton kbtnOk;
+        private KryptonButton kbtnUtiliseAsBaseColour;
+        private KryptonButton kbtnGenerateColour;
+        private System.Windows.Forms.Panel panel1;
+        private KryptonPanel kryptonPanel1;
+        private KryptonToolkitSuiteExtendedCore.CircularPictureBox circularPictureBox1;
+        private ExtendedColourControls.KryptonRedTrackBar kryptonRedTrackBar1;
+        private KryptonButton kbtnGenerate;
+
+        private void InitializeComponent()
+        {
+            this.kryptonPanel2 = new ComponentFactory.Krypton.Toolkit.KryptonPanel();
+            this.kbtnOptions = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.kcbBaseColour = new ComponentFactory.Krypton.Toolkit.KryptonColorButton();
+            this.kbtnDefineOtherColours = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.kbtnOk = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.kbtnUtiliseAsBaseColour = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.kbtnGenerateColour = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.kbtnGenerate = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.kryptonPanel1 = new ComponentFactory.Krypton.Toolkit.KryptonPanel();
+            this.circularPictureBox1 = new KryptonToolkitSuiteExtendedCore.CircularPictureBox();
+            this.kryptonRedTrackBar1 = new ExtendedColourControls.KryptonRedTrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel2)).BeginInit();
+            this.kryptonPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).BeginInit();
+            this.kryptonPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.circularPictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kryptonPanel2
+            // 
+            this.kryptonPanel2.Controls.Add(this.kbtnOptions);
+            this.kryptonPanel2.Controls.Add(this.kcbBaseColour);
+            this.kryptonPanel2.Controls.Add(this.kbtnDefineOtherColours);
+            this.kryptonPanel2.Controls.Add(this.kbtnOk);
+            this.kryptonPanel2.Controls.Add(this.kbtnUtiliseAsBaseColour);
+            this.kryptonPanel2.Controls.Add(this.kbtnGenerateColour);
+            this.kryptonPanel2.Controls.Add(this.kbtnGenerate);
+            this.kryptonPanel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.kryptonPanel2.Location = new System.Drawing.Point(0, 273);
+            this.kryptonPanel2.Name = "kryptonPanel2";
+            this.kryptonPanel2.Size = new System.Drawing.Size(1208, 45);
+            this.kryptonPanel2.TabIndex = 2;
+            // 
+            // kbtnOptions
+            // 
+            this.kbtnOptions.AutoSize = true;
+            this.kbtnOptions.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.kbtnOptions.Location = new System.Drawing.Point(711, 6);
+            this.kbtnOptions.Name = "kbtnOptions";
+            this.kbtnOptions.Size = new System.Drawing.Size(69, 30);
+            this.kbtnOptions.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kbtnOptions.TabIndex = 77;
+            this.kbtnOptions.Values.Text = "&Options";
+            // 
+            // kcbBaseColour
+            // 
+            this.kcbBaseColour.AutoSize = true;
+            this.kcbBaseColour.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.kcbBaseColour.Location = new System.Drawing.Point(506, 6);
+            this.kcbBaseColour.Name = "kcbBaseColour";
+            this.kcbBaseColour.Size = new System.Drawing.Size(199, 30);
+            this.kcbBaseColour.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kcbBaseColour.TabIndex = 76;
+            this.kcbBaseColour.Values.Text = "&Choose a Base Colour";
+            // 
+            // kbtnDefineOtherColours
+            // 
+            this.kbtnDefineOtherColours.AutoSize = true;
+            this.kbtnDefineOtherColours.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.kbtnDefineOtherColours.Location = new System.Drawing.Point(335, 6);
+            this.kbtnDefineOtherColours.Name = "kbtnDefineOtherColours";
+            this.kbtnDefineOtherColours.Size = new System.Drawing.Size(165, 30);
+            this.kbtnDefineOtherColours.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kbtnDefineOtherColours.TabIndex = 63;
+            this.kbtnDefineOtherColours.Values.Text = "Define Other &Colours";
+            // 
+            // kbtnOk
+            // 
+            this.kbtnOk.AutoSize = true;
+            this.kbtnOk.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.kbtnOk.Location = new System.Drawing.Point(1160, 6);
+            this.kbtnOk.Name = "kbtnOk";
+            this.kbtnOk.Size = new System.Drawing.Size(32, 30);
+            this.kbtnOk.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kbtnOk.TabIndex = 60;
+            this.kbtnOk.Values.Text = "O&k";
+            // 
+            // kbtnUtiliseAsBaseColour
+            // 
+            this.kbtnUtiliseAsBaseColour.AutoSize = true;
+            this.kbtnUtiliseAsBaseColour.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.kbtnUtiliseAsBaseColour.Location = new System.Drawing.Point(12, 6);
+            this.kbtnUtiliseAsBaseColour.Name = "kbtnUtiliseAsBaseColour";
+            this.kbtnUtiliseAsBaseColour.Size = new System.Drawing.Size(167, 30);
+            this.kbtnUtiliseAsBaseColour.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kbtnUtiliseAsBaseColour.TabIndex = 15;
+            this.kbtnUtiliseAsBaseColour.Values.Text = "Utilise as Base &Colour";
+            // 
+            // kbtnGenerateColour
+            // 
+            this.kbtnGenerateColour.AutoSize = true;
+            this.kbtnGenerateColour.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.kbtnGenerateColour.Location = new System.Drawing.Point(506, 6);
+            this.kbtnGenerateColour.Name = "kbtnGenerateColour";
+            this.kbtnGenerateColour.Size = new System.Drawing.Size(197, 30);
+            this.kbtnGenerateColour.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kbtnGenerateColour.TabIndex = 29;
+            this.kbtnGenerateColour.Values.Text = "Generate Random &Colour";
+            this.kbtnGenerateColour.Visible = false;
+            // 
+            // kbtnGenerate
+            // 
+            this.kbtnGenerate.AutoSize = true;
+            this.kbtnGenerate.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.kbtnGenerate.Location = new System.Drawing.Point(185, 6);
+            this.kbtnGenerate.Name = "kbtnGenerate";
+            this.kbtnGenerate.Size = new System.Drawing.Size(144, 30);
+            this.kbtnGenerate.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kbtnGenerate.TabIndex = 16;
+            this.kbtnGenerate.Values.Text = "Gener&ate a Colour";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 271);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1208, 2);
+            this.panel1.TabIndex = 3;
+            // 
+            // kryptonPanel1
+            // 
+            this.kryptonPanel1.Controls.Add(this.kryptonRedTrackBar1);
+            this.kryptonPanel1.Controls.Add(this.circularPictureBox1);
+            this.kryptonPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.kryptonPanel1.Location = new System.Drawing.Point(0, 0);
+            this.kryptonPanel1.Name = "kryptonPanel1";
+            this.kryptonPanel1.Size = new System.Drawing.Size(1208, 271);
+            this.kryptonPanel1.TabIndex = 4;
+            // 
+            // circularPictureBox1
+            // 
+            this.circularPictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.circularPictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.circularPictureBox1.Name = "circularPictureBox1";
+            this.circularPictureBox1.Size = new System.Drawing.Size(206, 195);
+            this.circularPictureBox1.TabIndex = 5;
+            this.circularPictureBox1.TabStop = false;
+            // 
+            // kryptonRedTrackBar1
+            // 
+            this.kryptonRedTrackBar1.DrawBackground = true;
+            this.kryptonRedTrackBar1.Location = new System.Drawing.Point(297, 62);
+            this.kryptonRedTrackBar1.Maximum = 255;
+            this.kryptonRedTrackBar1.Name = "kryptonRedTrackBar1";
+            this.kryptonRedTrackBar1.Size = new System.Drawing.Size(182, 27);
+            this.kryptonRedTrackBar1.StateCommon.Tick.Color1 = System.Drawing.Color.Red;
+            this.kryptonRedTrackBar1.StateCommon.Track.Color1 = System.Drawing.Color.Red;
+            this.kryptonRedTrackBar1.StateCommon.Track.Color2 = System.Drawing.Color.Red;
+            this.kryptonRedTrackBar1.StateCommon.Track.Color3 = System.Drawing.Color.Red;
+            this.kryptonRedTrackBar1.StateCommon.Track.Color4 = System.Drawing.Color.Red;
+            this.kryptonRedTrackBar1.StateCommon.Track.Color5 = System.Drawing.Color.Red;
+            this.kryptonRedTrackBar1.TabIndex = 5;
+            // 
+            // KryptonPaletteColourMixerBasic
+            // 
+            this.ClientSize = new System.Drawing.Size(1208, 318);
+            this.Controls.Add(this.kryptonPanel1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.kryptonPanel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "KryptonPaletteColourMixerBasic";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel2)).EndInit();
+            this.kryptonPanel2.ResumeLayout(false);
+            this.kryptonPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).EndInit();
+            this.kryptonPanel1.ResumeLayout(false);
+            this.kryptonPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.circularPictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+    }
+}
