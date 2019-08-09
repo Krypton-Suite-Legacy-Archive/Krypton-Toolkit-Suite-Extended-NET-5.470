ace12199491ed51ee9df00b0066e6587a8716bcb
Peter Wagner <peterwagner@live.co.uk>
2019-08-09 19:15:02 +0100

    * Files

---
+++ Source/Krypton Toolkit Suite Extended/Floating Toolbars/UX/RibbonContainerWindow.cs
@@ -0,0 +1,13 @@
+﻿using ComponentFactory.Krypton.Toolkit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloatingToolbars
+{
+    public class RibbonContainerWindow : KryptonForm
+    {
+    }
+}
