be5a3018521a77d0f1c5d50e84f34a61fed2632f
Peter Wagner <peterwagner@live.co.uk>
2019-08-12 16:17:49 +0100

    * Files

--- Source/Krypton Toolkit Suite Extended/Core/Enumerations/Enumerations.cs
+++ Source/Krypton Toolkit Suite Extended/Core/Enumerations/Enumerations.cs
@@ -41,6 +41,29 @@
         SAVEFILE
     }
 
+    /// <summary>
+    /// Text alignment options.
+    /// </summary>
+    public enum TextAlignment
+    {
+        /// <summary>
+        /// Align text to the left.
+        /// </summary>
+        LEFT = 1,
+        /// <summary>
+        /// Align text to the right.
+        /// </summary>
+        RIGHT = 2,
+        /// <summary>
+        /// Centre the text.
+        /// </summary>
+        CENTRE = 3,
+        /// <summary>
+        /// Justify the text.
+        /// </summary>
+        JUSTIFY = 4
+    }
+
     #region Scrollbars
     /// <summary>
     /// The scrollbar arrow button states.
