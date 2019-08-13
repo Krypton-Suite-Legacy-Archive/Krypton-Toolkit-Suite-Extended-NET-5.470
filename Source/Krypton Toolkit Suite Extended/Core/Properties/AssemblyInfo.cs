22609cf21a09bc2751af212b6dbf2347b67489b9
Peter Wagner <peterwagner@live.co.uk>
2019-08-13 09:04:31 +0100

    Update AssemblyInfo.cs

--- Source/Krypton Toolkit Suite Extended/Core/Properties/AssemblyInfo.cs
+++ Source/Krypton Toolkit Suite Extended/Core/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("5.470.1216.0")]
-[assembly: AssemblyFileVersion("5.570.1216.0")]
+[assembly: AssemblyFileVersion("5.470.1216.0")]
