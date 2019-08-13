033c82c0989a6d60f422d85e25cd50aea419a676
Peter Wagner <peterwagner@live.co.uk>
2019-08-13 19:14:49 +0100

    * Files

---
+++ Source/Krypton Toolkit Suite Extended/Extended Menu and Toolbar Items/Classes/ToolStripControlHostFixed.cs
@@ -0,0 +1,26 @@
+﻿#region BSD License
+/*
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE.md file or at
+ * https://github.com/Wagnerp/Krypton-Toolkit-Suite-Extended-NET-5.470/blob/master/LICENSE
+ *
+ */
+#endregion
+
+using System.Windows.Forms;
+
+namespace ExtendedMenuToolbarItems
+{
+    public class ToolStripControlHostFixed : ToolStripControlHost
+    {
+        public ToolStripControlHostFixed() : base(new Control())
+        {
+
+        }
+
+        public ToolStripControlHostFixed(Control c) : base(c)
+        {
+
+        }
+    }
+}
