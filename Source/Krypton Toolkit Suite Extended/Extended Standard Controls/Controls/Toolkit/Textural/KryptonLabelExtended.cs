f3d093c5c9b6f6b28a7841cd5959f25bb1726c93
Peter Wagner <peterwagner@live.co.uk>
2019-08-18 12:21:54 +0100

    * Files

---
+++ Source/Krypton Toolkit Suite Extended/Extended Standard Controls/Controls/Toolkit/Textural/KryptonLabelExtended.cs
@@ -0,0 +1,79 @@
+﻿using ComponentFactory.Krypton.Toolkit;
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ExtendedStandardControls
+{
+    [ToolboxBitmap(typeof(KryptonLabel))]
+    public class KryptonLabelExtended : KryptonLabel
+    {
+        #region Variables
+        private Color _textColourOne, _textColourTwo;
+
+        private Font _typeface;
+
+        private Image _image;
+        #endregion
+
+        #region Properties
+        [Category("Appearance"), Description("The first text colour.")]
+        public Color TextColourOne { get => _textColourOne; set { _textColourOne = value; Invalidate(); } }
+
+        [Category("Appearance"), Description("The second text colour.")]
+        public Color TextColourTwo { get => _textColourTwo; set { _textColourTwo = value; Invalidate(); } }
+
+        [Category("Appearance"), Description("The typeface of the text box.")]
+        public Font Typeface { get => _typeface; set { _typeface = value; Invalidate(); } }
+
+        [Category("Appearance"), Description("The image.")]
+        public Image Image { get => _image; set { _image = value; Invalidate(); } }
+        #endregion
+
+        #region Constructor
+        public KryptonLabelExtended()
+        {
+            TextColourOne = Color.Empty;
+
+            TextColourTwo = Color.Empty;
+
+            Typeface = null;
+
+            Image = null;
+
+            UpdateAppearanceValues(TextColourOne, TextColourTwo, Typeface, Image);
+        }
+        #endregion
+
+        #region Method
+        private void UpdateAppearanceValues(Color textColourOne, Color textColourTwo, Font typeface, Image image)
+        {
+            StateCommon.LongText.Color1 = textColourOne;
+
+            StateCommon.LongText.Color2 = textColourTwo;
+
+            StateCommon.LongText.Font = typeface;
+
+            StateCommon.LongText.Image = image;
+
+            StateCommon.ShortText.Color1 = textColourOne;
+
+            StateCommon.ShortText.Color2 = textColourTwo;
+
+            StateCommon.ShortText.Font = typeface;
+
+            StateCommon.ShortText.Image = image;
+        }
+        #endregion
+
+        #region Override
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            UpdateAppearanceValues(TextColourOne, TextColourTwo, Typeface, Image);
+
+            base.OnPaint(e);
+        }
+        #endregion
+    }
+}
--- Source/Krypton Toolkit Suite Extended/Playground/StandardControlsExtended.cs
+++ Source/Krypton Toolkit Suite Extended/Playground/StandardControlsExtended.cs
@@ -5,33 +5,73 @@ namespace Playground
 {
     public class StandardControlsExtended : KryptonForm
     {
-        private KryptonHeaderGroup kryptonHeaderGroup1;
+        private KryptonLinkLabel kryptonLinkLabel1;
+        private ExtendedStandardControls.KryptonLabelExtended kryptonLabelExtended1;
+        private KryptonListBox kryptonListBox1;
+        private KryptonLabel kryptonLabel1;
 
         private void InitializeComponent()
         {
-            this.kryptonHeaderGroup1 = new ComponentFactory.Krypton.Toolkit.KryptonHeaderGroup();
-            ((System.ComponentModel.ISupportInitialize)(this.kryptonHeaderGroup1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.kryptonHeaderGroup1.Panel)).BeginInit();
-            this.kryptonHeaderGroup1.SuspendLayout();
+            this.kryptonLabel1 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.kryptonLinkLabel1 = new ComponentFactory.Krypton.Toolkit.KryptonLinkLabel();
+            this.kryptonLabelExtended1 = new ExtendedStandardControls.KryptonLabelExtended();
+            this.kryptonListBox1 = new ComponentFactory.Krypton.Toolkit.KryptonListBox();
             this.SuspendLayout();
             // 
-            // kryptonHeaderGroup1
+            // kryptonLabel1
             // 
-            this.kryptonHeaderGroup1.Location = new System.Drawing.Point(0, 0);
-            this.kryptonHeaderGroup1.Name = "kryptonHeaderGroup1";
-            this.kryptonHeaderGroup1.Size = new System.Drawing.Size(150, 150);
-            this.kryptonHeaderGroup1.TabIndex = 0;
+            this.kryptonLabel1.Location = new System.Drawing.Point(376, 326);
+            this.kryptonLabel1.Name = "kryptonLabel1";
+            this.kryptonLabel1.Size = new System.Drawing.Size(88, 20);
+            this.kryptonLabel1.TabIndex = 0;
+            this.kryptonLabel1.Values.Text = "kryptonLabel1";
+            // 
+            // kryptonLinkLabel1
+            // 
+            this.kryptonLinkLabel1.Location = new System.Drawing.Point(623, 360);
+            this.kryptonLinkLabel1.Name = "kryptonLinkLabel1";
+            this.kryptonLinkLabel1.Size = new System.Drawing.Size(110, 20);
+            this.kryptonLinkLabel1.TabIndex = 1;
+            this.kryptonLinkLabel1.Values.Text = "kryptonLinkLabel1";
+            // 
+            // kryptonLabelExtended1
+            // 
+            this.kryptonLabelExtended1.Image = global::Playground.Properties.Resources.KR_32_x_32_Orange;
+            this.kryptonLabelExtended1.Location = new System.Drawing.Point(430, 148);
+            this.kryptonLabelExtended1.Name = "kryptonLabelExtended1";
+            this.kryptonLabelExtended1.Size = new System.Drawing.Size(234, 29);
+            this.kryptonLabelExtended1.StateCommon.LongText.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.kryptonLabelExtended1.StateCommon.LongText.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.kryptonLabelExtended1.StateCommon.LongText.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kryptonLabelExtended1.StateCommon.LongText.Image = global::Playground.Properties.Resources.KR_32_x_32_Orange;
+            this.kryptonLabelExtended1.StateCommon.ShortText.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.kryptonLabelExtended1.StateCommon.ShortText.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.kryptonLabelExtended1.StateCommon.ShortText.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kryptonLabelExtended1.StateCommon.ShortText.Image = global::Playground.Properties.Resources.KR_32_x_32_Orange;
+            this.kryptonLabelExtended1.TabIndex = 2;
+            this.kryptonLabelExtended1.TextColourOne = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.kryptonLabelExtended1.TextColourTwo = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.kryptonLabelExtended1.Typeface = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kryptonLabelExtended1.Values.Text = "kryptonLabelExtended1";
+            // 
+            // kryptonListBox1
+            // 
+            this.kryptonListBox1.Location = new System.Drawing.Point(355, 395);
+            this.kryptonListBox1.Name = "kryptonListBox1";
+            this.kryptonListBox1.Size = new System.Drawing.Size(120, 96);
+            this.kryptonListBox1.TabIndex = 3;
             // 
             // StandardControlsExtended
             // 
             this.ClientSize = new System.Drawing.Size(1369, 636);
-            this.Controls.Add(this.kryptonHeaderGroup1);
+            this.Controls.Add(this.kryptonListBox1);
+            this.Controls.Add(this.kryptonLabelExtended1);
+            this.Controls.Add(this.kryptonLinkLabel1);
+            this.Controls.Add(this.kryptonLabel1);
             this.Name = "StandardControlsExtended";
             this.Load += new System.EventHandler(this.StandardControlsExtended_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.kryptonHeaderGroup1.Panel)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.kryptonHeaderGroup1)).EndInit();
-            this.kryptonHeaderGroup1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
