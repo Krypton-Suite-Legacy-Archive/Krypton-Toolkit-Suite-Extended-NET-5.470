e507c6c9be3dadb58fc8150c76d00e1fa44dde43
Peter Wagner <peterwagner@live.co.uk>
2019-08-18 12:53:03 +0100

    * #89

--- Source/Krypton Toolkit Suite Extended/Extended Standard Controls/Controls/Toolkit/Textural/KryptonLinkLabelExtended.cs
+++ Source/Krypton Toolkit Suite Extended/Extended Standard Controls/Controls/Toolkit/Textural/KryptonLinkLabelExtended.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ComponentFactory.Krypton.Toolkit;
+using System.Drawing;
 
-namespace ExtendedStandardControls.Controls.Toolkit.Textural
+namespace ExtendedStandardControls
 {
-    class KryptonLinkLabelExtended
+    [ToolboxBitmap(typeof(KryptonLinkLabel))]
+    public class KryptonLinkLabelExtended : KryptonLinkLabel
     {
+        #region Variables
+        private Color _overrideFocusLongTextColourOne, _overrideFocusLongTextColourTwo, _overrideFocusShortTextColourOne, _overrideFocusShortTextColourTwo, _overrideNotVisitedLongTextColourOne, _overrideNotVisitedLongTextColourTwo, _overrideNotVisitedShortTextColourOne, _overrideNotVisitedShortTextColourTwo, _overridePressedLongTextColourOne, _overridePressedLongTextColourTwo, _overridePressedShortTextColourOne, _overridePressedShortTextColourTwo, _overrideVisitedLongTextColourOne, _overrideVisitedLongTextColourTwo, _overrideVisitedShortTextColourOne, _overrideVisitedShortTextColourTwo, _commonLongTextColourOne, _commonLongTextColourTwo, _commonShortTextColourOne, _commonShortTextColourTwo;
+
+        private Font _typeface;
+        #endregion
+
+        #region Properties
+
+        #region Override Focus
+        public Color OverrideFocusLongTextColourOne { get => _overrideFocusLongTextColourOne; set { _overrideFocusLongTextColourOne = value; Invalidate(); } }
+
+        public Color OverrideFocusLongTextColourTwo { get => _overrideFocusLongTextColourTwo; set { _overrideFocusLongTextColourTwo = value; Invalidate(); } }
+
+        public Color OverrideFocusShortTextColourOne { get => _overrideFocusShortTextColourOne; set { _overrideFocusShortTextColourOne = value; Invalidate(); } }
+
+        public Color OverrideFocusShortTextColourTwo { get => _overrideFocusShortTextColourTwo; set { _overrideFocusShortTextColourTwo = value; Invalidate(); } }
+        #endregion
+
+        #endregion
     }
 }
