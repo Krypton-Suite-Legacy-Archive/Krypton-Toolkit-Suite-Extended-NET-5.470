417c44cf591e24ccdec322c80f8f0e58641e79c5
Peter Wagner <peterwagner@live.co.uk>
2019-09-17 16:25:17 +0100

    * Files

--- Source/Krypton Toolkit Suite Extended/Krypton Backup Utility/Classes/Copy.cs
+++ Source/Krypton Toolkit Suite Extended/Krypton Backup Utility/Classes/Copy.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KryptonBackupUtility.Classes
+﻿namespace KryptonBackupUtility.Classes
 {
-    class Copy
+    public class Copy
     {
     }
 }
