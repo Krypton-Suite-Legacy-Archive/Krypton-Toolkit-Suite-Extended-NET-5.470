9e5d306218cb2074101c577ad22039dd9a6bc0e3
Peter Wagner <peterwagner@live.co.uk>
2019-12-29 19:19:47 +0000

    * Files

--- Source/Krypton Toolkit Suite Extended/Full Toolkit/Application Updater/Properties/AssemblyInfo.cs
+++ Source/Krypton Toolkit Suite Extended/Full Toolkit/Application Updater/Properties/AssemblyInfo.cs
@@ -42,7 +42,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.470.789.0")]
-[assembly: AssemblyFileVersion("5.470.789.0")]
+[assembly: AssemblyVersion("5.470.792.0")]
+[assembly: AssemblyFileVersion("5.470.792.0")]
 [assembly: NeutralResourcesLanguage("en-GB")]
 
